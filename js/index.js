ff6e603b2336e856f0122e755e125a8fa4446e70
fabulous-fabio <fabio.scagliola@gmx.de>
2024-05-18 11:33:22 +0200

    Design and Context changes

--- js/index.js
+++ js/index.js
@@ -10,3 +10,27 @@ navLinks.forEach(link => {
     document.body.classList.remove('nav-open');
   })
 })
+
+$(document).ready(function() {
+  $('.carousel-container').each(function() {
+    var $carousel = $(this).find('.about-me__img');
+    var $items = $carousel.find('.item');
+    var $nextBtn = $(this).find('.next-btn');
+
+    // Initial set the first item as active
+    $items.first().addClass('active');
+
+    $nextBtn.on('click', function() {
+      var $current = $items.filter('.active');
+      var $next = $current.next('.item');
+
+      // If there is no next item, start from the first item
+      if ($next.length === 0) {
+        $next = $items.first();
+      }
+
+      $current.removeClass('active');
+      $next.addClass('active');
+    });
+  });
+});
